c6671b93760012090ce3d1590213d12d20ac3b76
Guy Marshall <guymarshall9@gmail.com>
2022-10-16 20:20:43 +0100

    Comment out unused velocity variable for now

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         final double speedOfLight = 299792458;
 
         double restMass = Functions.userInput("Enter rest mass (kg): ");
-        double velocity = Functions.userInput("Enter velocity (ms^-1): ");
+//        double velocity = Functions.userInput("Enter velocity (ms^-1): ");
 
         for (int i = 1000000; i < speedOfLight; i++) {
             if (i % 1000 == 0) {
