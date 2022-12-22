f9cdfd6bbd2a84437378d6486d9761d2fbb57f2a
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-22 12:30:56 +0000

    Add package to top Functions.java + move curly braces to new line

--- src/main/java/com/marshall/guy/Functions.java
+++ src/main/java/com/marshall/guy/Functions.java
@@ -1,9 +1,13 @@
+package com.marshall.guy;
+
 import java.util.Scanner;
 
-public class Functions {
+public class Functions
+{
     static Scanner scanner = new Scanner(System.in);
 
-    public static double relativisticMass(double restMass, double velocity, double speedOfLight) {
+    public static double relativisticMass(double restMass, double velocity, double speedOfLight)
+    {
         return restMass / Math.sqrt(
                 1 - (
                         (velocity * velocity) / (speedOfLight * speedOfLight)
@@ -11,15 +15,18 @@ public class Functions {
         );
     }
 
-    public static double kineticEnergy(double mass, double velocity) {
+    public static double kineticEnergy(double mass, double velocity)
+    {
         return 0.5 * mass * velocity * velocity;
     }
 
-    public static double energy(double mass, double speedOfLight) {
+    public static double energy(double mass, double speedOfLight)
+    {
         return mass * speedOfLight * speedOfLight;
     }
 
-    public static double userInput(String prompt) {
+    public static double userInput(String prompt)
+    {
         System.out.print(prompt);
         return scanner.nextDouble();
     }
